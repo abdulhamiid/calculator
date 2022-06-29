ea7633c8d772338068e63d8427f67eb4f4ef30c0
Abdulhamid <adioabdulhamid@yahoo.com>
2022-06-29 15:05:40 +0100

    Add class-based component instead function-basedas required

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import React, { Component } from 'react';
 import Calculate from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculate />
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculate />
+      </div>
+    );
+  }
 }
 
 export default App;
